3be21163265d91ede1048955e9d4c24c5f461450
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-06-27 09:07:05 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""defines a square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """Init of a square.
+
+        Args:
+            size(int): size of a new square"""
+        self.__size = size
