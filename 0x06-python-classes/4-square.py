628814b5ccf6e2d8af9298109fc86945347b8165
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-06-27 10:04:30 -0700

    fixing code 2

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """size setter"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
