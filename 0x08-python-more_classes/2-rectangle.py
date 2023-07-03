05f03f755c5d2f8af17a2bafdb8aed92d8655517
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-07-03 16:56:58 -0700

    fixing unknown error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,11 +38,9 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-
     def area(self):
         """returns the area of the rectangle"""
         return (self.__width * self.__height)
-
 
     def perimeter(self):
         """returns the perimeter of the rectangle"""
