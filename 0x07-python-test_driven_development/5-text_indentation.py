4a4c16d05bb8bd35c5d926ab6cf96c1de4a598dd
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-07-04 13:03:51 -0700

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    """Prints the given text with 2 new lines after each occurrence of '.', '?', and ':' characters.
+
+    Args:
+        text (str): The text to be indented.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
