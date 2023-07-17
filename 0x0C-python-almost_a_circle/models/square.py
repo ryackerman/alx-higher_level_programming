93498c3ccb1493d1d36681883707224457b29ce4
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-07-16 18:50:22 -0700

    10. And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines the Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Init a Square instance
+        Args:
+            size (int): The size of the square.
+            x (int): Optional. The x-coordinate of the square's position.
+            y (int): Optional. The y-coordinate of the square's position.
+            id (int): Optional. The id value for the instance.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overrides the __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
