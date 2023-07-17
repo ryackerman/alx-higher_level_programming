eec4e45d8c60d0746f2fd3d9f0b13d875f82955d
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-07-17 13:34:24 -0700

    trying to figure out what's the prob

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Defines the Rectangle class
-"""
+"""Defines the Rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Rectangle class inherits from Base
-    """
+    """Rectangle class inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
