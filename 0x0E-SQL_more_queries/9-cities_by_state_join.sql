3e63013973a79465f9731f80379daf6626336d9d
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-08-16 17:54:57 -0700

    fixing query

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities` JOIN `states` ON `cities`.`state_id`=`states`,`id` ORDER BY `cities`.`id`;
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` c JOIN `states` s ON c.`state_id`=s.`id` ORDER BY c.`id`;
