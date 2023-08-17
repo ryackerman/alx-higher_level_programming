9e72088608d8edbe5b483d3088a2bed66fb782ab
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-08-17 05:49:32 -0700

    fixing query

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,2 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT `title`, SUM(`tv_show_ratings`.`rate`) `ratingSum` FROM `tv_shows` ts LEFT JOIN `tv_show_ratings` tsr ON tsr.`show_id`=ts.`id` GROUP BY `title` ORDER BY ratingSum DESC;
+SELECT `title`, SUM(`tv_show_ratings`.`rate`) 'rating' FROM `tv_shows` ts LEFT JOIN `tv_show_ratings` tsr ON tsr.`show_id` = ts.`id` GROUP BY `title` ORDER BY rating DESC;
