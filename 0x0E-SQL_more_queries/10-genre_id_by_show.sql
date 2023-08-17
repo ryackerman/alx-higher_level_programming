cee4b21052bbdc18e754a7f8eb45d49182c23de3
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-08-16 18:01:01 -0700

    fixing table name

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT ts.`title`, tsg.`genre_id` FROM `tv_show` ts JOIN `tv_show_genres` tsg ON ts.`id`=tsg.`show_id` ORDER BY ts.`title`, tsg.`genre_id`;
+SELECT ts.`title`, tsg.`genre_id` FROM `tv_shows` ts JOIN `tv_show_genres` tsg ON ts.`id`=tsg.`show_id` ORDER BY ts.`title`, tsg.`genre_id`;
