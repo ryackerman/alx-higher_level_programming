496a200ad8973a67c3a9ec9198cd8c1c66b245b2
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-08-16 17:59:45 -0700

    10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT ts.`title`, tsg.`genre_id` FROM `tv_show` ts JOIN `tv_show_genres` tsg ON ts.`id`=tsg.`show_id` ORDER BY ts.`title`, tsg.`genre_id`;
