389f6c03b1f0b5470b8a9871876856fb9437863d
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-08-17 04:54:30 -0700

    fixing SQL keyword

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tg.`name` from `tv_genres` tg JOIN `tv_show_genres` tsg ON tg.`id`=tsg.`genre_id` JOIN `tv_shows` ts ON ts.`id`=tsg.`show_id` WHERE ts.`title`="Dexter" ORDER BY tg.`name`;
+SELECT tg.`name` FROM `tv_genres` tg JOIN `tv_show_genres` tsg ON tg.`id`=tsg.`genre_id` JOIN `tv_shows` ts ON ts.`id`=tsg.`show_id` WHERE ts.`title`="Dexter" ORDER BY tg.`name`;
