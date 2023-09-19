88706a947fe3cf203349f5b13e3e2574ec92e397
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-09-18 17:17:57 -0700

    fixing pycodestyle validation

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""takes in the name of a state as an argument and lists all cities of that state"""
+"""
+takes in the name of a state as an argument and lists all cities of that state
+"""
 import MySQLdb
 import sys
 
