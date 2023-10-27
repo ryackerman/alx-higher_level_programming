61809be045d42875e6a626acc6c02f9a09a43c02
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-10-27 14:52:16 -0700

    fixing code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
   if (error)
 	console.log(error);
 });
